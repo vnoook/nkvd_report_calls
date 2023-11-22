d4121aed9dd4cb9c7074977e4723c7ef492d8f2c
noook <noook@yandex.ru>
2023-11-22 13:11:31 +0700

    #46 ref 409

--- nkvd_report_calls_gui.py
+++ nkvd_report_calls_gui.py
@@ -236,10 +236,10 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
         )
 
         # номера колонок для сбора данных
-        wb_in_s_col_1 = 7   # Отделение
+        wb_in_s_col_1 = 7  # Отделение
         wb_in_s_col_2 = 18  # Записавшая организация
         wb_in_s_col_3 = 19  # Кем записан
-        wb_in_s_col_4 = 9   # Услуга
+        wb_in_s_col_4 = 9  # Услуга
         wb_in_s_col_5 = 10  # Статус услуги
 
         # открывается свежий файл
@@ -253,16 +253,12 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
         # получение всех данных из файла и его закрытие, чтобы к нему больше не возвращаться
         for row in range(wb_in_s_row_begin, wb_in_s_row_end + 1):
             list_all_data_fresh.append([wb_in_s.cell(row=row, column=wb_in_s_col_1).value,
-                                  wb_in_s.cell(row=row, column=wb_in_s_col_2).value,
-                                  wb_in_s.cell(row=row, column=wb_in_s_col_3).value,
-                                  wb_in_s.cell(row=row, column=wb_in_s_col_4).value,
-                                  wb_in_s.cell(row=row, column=wb_in_s_col_5).value
-                                  ])
+                                        wb_in_s.cell(row=row, column=wb_in_s_col_2).value,
+                                        wb_in_s.cell(row=row, column=wb_in_s_col_3).value,
+                                        wb_in_s.cell(row=row, column=wb_in_s_col_4).value,
+                                        wb_in_s.cell(row=row, column=wb_in_s_col_5).value
+                                        ])
         wb_in.close()
-
-        # создание отчёта в xlsx и активация рабочего листа
-        wb_out = openpyxl.Workbook()
-        wb_out_s = wb_out.active
 
         # открывается старый файл
         wb_in = openpyxl.load_workbook(file_xlsx_old)
@@ -281,13 +277,6 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                                       wb_in_s.cell(row=row, column=wb_in_s_col_5).value
                                       ])
         wb_in.close()
-
-        # print()
-        # print(*list_all_data_old)
-        # print()
-        # print(*list_all_data_fresh)
-        # print()
-        print(f'{self.checkBox_short.isEnabled() = }')
 
         # создание нового отчёта в xlsx и активация рабочего листа
         wb_out = openpyxl.Workbook()
@@ -310,7 +299,8 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                         if dict_organization_fresh[val_str[0]].get(val_str[1]) is None:
                             dict_organization_fresh[val_str[0]][val_str[1]] = 1
                         else:
-                            dict_organization_fresh[val_str[0]][val_str[1]] = dict_organization_fresh[val_str[0]][val_str[1]] + 1
+                            dict_organization_fresh[val_str[0]][val_str[1]] = \
+                                dict_organization_fresh[val_str[0]][val_str[1]] + 1
 
                 # заполнение словаря "кем записан"
                 if val_str[2] in str_person_summ:
@@ -321,7 +311,8 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                             if dict_persona_fresh[val_str[0]].get(val_str[2]) is None:
                                 dict_persona_fresh[val_str[0]][val_str[2]] = 1
                             else:
-                                dict_persona_fresh[val_str[0]][val_str[2]] = dict_persona_fresh[val_str[0]][val_str[2]] + 1
+                                dict_persona_fresh[val_str[0]][val_str[2]] = \
+                                    dict_persona_fresh[val_str[0]][val_str[2]] + 1
 
                 # # заполнение словаря "услуги"
                 # if dict_service.get(val_str[0]) is None:
@@ -340,7 +331,8 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                         if dict_status_service_fresh[val_str[0]].get(val_str[4]) is None:
                             dict_status_service_fresh[val_str[0]][val_str[4]] = 1
                         else:
-                            dict_status_service_fresh[val_str[0]][val_str[4]] = dict_status_service_fresh[val_str[0]][val_str[4]] + 1
+                            dict_status_service_fresh[val_str[0]][val_str[4]] = \
+                                dict_status_service_fresh[val_str[0]][val_str[4]] + 1
 
         # подсчёт и распределение старых данных
         for val_str in list_all_data_old:
@@ -359,7 +351,8 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                         if dict_organization_old[val_str[0]].get(val_str[1]) is None:
                             dict_organization_old[val_str[0]][val_str[1]] = 1
                         else:
-                            dict_organization_old[val_str[0]][val_str[1]] = dict_organization_old[val_str[0]][val_str[1]] + 1
+                            dict_organization_old[val_str[0]][val_str[1]] = \
+                                dict_organization_old[val_str[0]][val_str[1]] + 1
 
                 # заполнение словаря "кем записан"
                 if val_str[2] in str_person_summ:
@@ -370,7 +363,8 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                             if dict_persona_old[val_str[0]].get(val_str[2]) is None:
                                 dict_persona_old[val_str[0]][val_str[2]] = 1
                             else:
-                                dict_persona_old[val_str[0]][val_str[2]] = dict_persona_old[val_str[0]][val_str[2]] + 1
+                                dict_persona_old[val_str[0]][val_str[2]] = \
+                                    dict_persona_old[val_str[0]][val_str[2]] + 1
 
                 # # заполнение словаря "услуги"
                 # if dict_service.get(val_str[0]) is None:
@@ -389,7 +383,8 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
                         if dict_status_service_old[val_str[0]].get(val_str[4]) is None:
                             dict_status_service_old[val_str[0]][val_str[4]] = 1
                         else:
-                            dict_status_service_old[val_str[0]][val_str[4]] = dict_status_service_old[val_str[0]][val_str[4]] + 1
+                            dict_status_service_old[val_str[0]][val_str[4]] = \
+                                dict_status_service_old[val_str[0]][val_str[4]] + 1
 
         # сортировка словарей свежих
         dict_organization_fresh = dict(sorted(dict_organization_fresh.items()))
@@ -404,10 +399,6 @@ class WindowMain(PyQt5.QtWidgets.QMainWindow):
         dict_persona_old = dict(sorted(dict_persona_old.items()))
         # dict_service = dict(sorted(dict_service.items()))
         dict_status_service_old = dict(sorted(dict_status_service_old.items()))
-
-        # print(*dict_status_service_old.values(), sep='\n')
-        # print(dict_status_service_old, sep='\n')
-        # exit()
 
         # добавления стиля строк
         style1 = openpyxl.styles.Font(bold=True, size=18)
